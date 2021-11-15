17b64d1b00c7b2ef77283bc99c5b366e44ab3579
Uzair khan <muhammad.uzair@cachecloud.com>
2021-11-15 11:59:44 +0100

    updating unit tests

--- tests/testthat/test_coronaApi.R
+++ tests/testthat/test_coronaApi.R
@@ -1,31 +1,46 @@
 library(httr)
 obj <- coronaApi()
-
+obj$getDailyReportedData('daily')
 
 test_that("Type checking",{
   expect_type(obj$getSingleCountryList("sweden"), "list")
 
   expect_type(obj$getOverAllList(), "list")
 
-  expect_type(obj$getOverAllList(), "list")
+  expect_type(obj$getDailyReportedData('daily'), "list")
+})
 
-  expect_type(obj$getOverAllList(), "list")
+test_that("should always return greater than zero", {
+  expect_true(length(obj$getOverAllList())!=0)
+  expect_true(length(obj$getDailyReportedData('daily'))!=0)
+  expect_true(length(obj$getSingleCountryList("sweden"))!=0)
+
+})
+
+test_that("internal structure of getInfoStation function output is correct", {
+  cols  <- obj$getOverAllList()
+
+  attrib_cols<-c("confirmed" , "recovered"  ,  "deaths" )
+  expect_equal(attributes(cols)[[1]],attrib_cols)
+
+  cols_swed  <- obj$getSingleCountryList("sweden")
+  attrib_swed<-c("confirmed" , "recovered"  ,  "deaths" )
+  expect_equal(attributes(cols_swed)[[1]],attrib_swed)
+
+  cols_daily  <- obj$getDailyReportedData("daily")
+  attrib_daily<-c("totalConfirmed" , "reportDate" )
+  expect_equal(attributes(cols_daily)[[1]],attrib_daily)
+
+
 })
 
 test_that("expect error to be returned",{
   input <-  "ads"
   expect_error(obj$getSingleCountryList(input))
-  input_error <-c(1,"2015-06-06-21-00",1)
 
-  expect_error(obj$getSingleCountryList(input))
-
-  expect_error(obj$getOverAllList("test"))
+  expect_error(obj$getDailyReportedData(input))
 
   expect_error(obj$getOverAllList(input))
-
-  input_error <-c(1,"pakistan")
-
-  expect_error(obj$getSingleCountryList(input))
 })
 
 
